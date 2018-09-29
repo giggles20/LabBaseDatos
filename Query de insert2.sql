686995b3c72f34903446ed101354c21afafaf738
giggles20 <42894891+giggles20@users.noreply.github.com>
2018-09-28 23:47:16 -0500

    Add files via upload

---
+++ Query de insert2.sql
@@ -0,0 +1,90 @@
+USE [PuntoDVenta]
+GO
+
+INSERT INTO [dbo].[Producto]
+           ([vDescripcion_Producto]
+           ,[fPrecio_Venta]
+           ,[fDescuento])
+     VALUES
+           ( 'refresco', 20.31, 0)
+GO
+
+INSERT INTO [dbo].[Producto]
+           ([vDescripcion_Producto]
+           ,[fPrecio_Venta]
+           ,[fDescuento])
+     VALUES
+           ( 'papas fritas', 10, 0)
+GO
+
+INSERT INTO [dbo].[Producto]
+           ([vDescripcion_Producto]
+           ,[fPrecio_Venta]
+           ,[fDescuento])
+     VALUES
+           ( 'chicles', 6, 0)
+GO
+
+INSERT INTO [dbo].[Producto]
+           ([vDescripcion_Producto]
+           ,[fPrecio_Venta]
+           ,[fDescuento])
+     VALUES
+           ( 'chocolate', 10, 0)
+GO
+
+INSERT INTO [dbo].[Producto]
+           ([vDescripcion_Producto]
+           ,[fPrecio_Venta]
+           ,[fDescuento])
+     VALUES
+           ( 'dona', 15.1, 0)
+GO
+
+INSERT INTO [dbo].[Producto]
+           ([vDescripcion_Producto]
+           ,[fPrecio_Venta]
+           ,[fDescuento])
+     VALUES
+           ( 'cafe de cafetera', 14.6, 0)
+GO
+
+INSERT INTO [dbo].[Producto]
+           ([vDescripcion_Producto]
+           ,[fPrecio_Venta]
+           ,[fDescuento])
+     VALUES
+           ( 'sandwich partido en dos', 24, 0)
+GO
+
+INSERT INTO [dbo].[Producto]
+           ([vDescripcion_Producto]
+           ,[fPrecio_Venta]
+           ,[fDescuento])
+     VALUES
+           ( 'termo', 30, 0)
+GO
+
+INSERT INTO [dbo].[Producto]
+           ([vDescripcion_Producto]
+           ,[fPrecio_Venta]
+           ,[fDescuento])
+     VALUES
+           ( 'lentes de sol', 120, 0)
+GO
+
+INSERT INTO [dbo].[Producto]
+           ([vDescripcion_Producto]
+           ,[fPrecio_Venta]
+           ,[fDescuento])
+     VALUES
+           ( 'gomas', 12 , 0)
+GO
+
+INSERT INTO [dbo].[Producto]
+           ([vDescripcion_Producto]
+           ,[fPrecio_Venta]
+           ,[fDescuento])
+     VALUES
+           ( 'agua', 13.68, 0)
+GO
